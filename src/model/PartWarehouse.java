a250be6d22ad3f888c7051fde47e6f05779e0cd0
brennanlaird <brennan.laird@gmail.com>
2021-05-16 23:22:03 -0600

    Final changes to the parts_warehouse branch 5-16-2021 11:21pm

--- src/model/PartWarehouse.java
+++ src/model/PartWarehouse.java
@@ -5,42 +5,33 @@ package model;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.util.ArrayList;
+
 
 public class PartWarehouse {
     private static ObservableList<Part> allParts = FXCollections.observableArrayList();
 
 
-    //Part[] partArray = new Part[10];
-    //int partStored=0;
 
-    //ArrayList<Part> partList = new ArrayList<Part>();
 
 
     //Constructor
     public PartWarehouse () {
-        //ArrayList<Part> partList = new ArrayList<Part>();
 
-        //System.out.println();
         System.out.println("The part warehouse constructor ran");
     }
 
-    //static method to add parts to the warehouse
+    //static method to add parts to the warehouse by adding them to the observable list object
     public static void stockPartWarehouse(Part newPart) {
-        //partArray[partStored++] = new InHouse();
-        //partList.add(newPart);
+
         allParts.add(newPart);
     }
-
-    //write code to use a data structure to store each object
-    //this should only run once when the program launches
-
 
 
     public static void newPartID() {
         //method to get a unique part id
     }
 
+    //This returns all the parts in the observable parts list for display in the main form
     public static ObservableList<Part> getAllParts(){
         return allParts;
     }
